229cec2a633338c8d4fd62ac4ed19d69fcc2882e
Cora Allen-Coleman <6508099+coraallencoleman@users.noreply.github.com>
2018-11-06 16:55:25 -0600

    added gitignore

--- spectralClustering.py
+++ spectralClustering.py
@@ -9,27 +9,18 @@ M2 = np.loadtxt("monkeyM2.txt")
 M2.shape
 gender = np.loadtxt("gender.txt", dtype=str)
 
-
-D = sum(M1) #degree matrix by summing over all columns to get a vector
-D = np.diag(D) #to make it into a matrix 35 diagonal entries correspond t
+D = sum(M1).diag #degree matrix
 lap = D-M1 #laplacian matrix
-[e, v] = np.linalg.eig(lap) #e = eigenvectors, v is diagonal matrix of eigenvalues
+[e, v] = np.linalg.eig(lap) #e = eigenvectors, v is a diagonal matrix of eigenvalues
 vv = np.diag(v) #vector of eigenvalues
 
 kmeans = KMeans(n_clusters=3, random_state=0).fit(M1)
 kmeans.labels_
 kmeans.cluster_centers_
 
+#visualizations
 figure(figsize=(10,5))
 subplot(121)
 title('Original unclustered data')
 plot(M1[:,0], M1[:,1], '.')
 xlabel('$x_1$'); ylabel('$x_2$')
-
-#small example
-X = np.array([[1, 2], [1, 4], [1, 0],
-    [4, 2], [4, 4], [4, 0]])
-kmeans_ex = KMeans(n_clusters=3, random_state=0).fit(X)
-kmeans_ex.labels_
-kmeans_ex.predict([[0, 0], [4, 4]])
-kmeans_ex.cluster_centers_
